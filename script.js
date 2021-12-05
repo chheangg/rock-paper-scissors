649177ecf7ab17122d4cff625a5ce1d91afe0441
chheangg <balldremz@gmail.com>
2021-12-05 10:46:40 +0700

    include method for evaluating rounds
    In the js file, computerPlayer function was split into computerPlay and
    computerSelection to seperate the method for generating random choices
    and assigning the choice
    
    playerSelection function was added to verify the correct choices available and
    playerRound was added to evaluating the outcome of the round (Win or
    False)

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
-function computerPlayer() {
+let choice = "rock";
+let computer = computerSelection();
+
+
+
+function computerPlay() {
     let rollDice = Math.floor(Math.random() * 3)
-    let computerChoice;
-    if ( rollDice == 0 ) {
-        return computerChoice = 'Rock';
-    } else if ( rollDice == 1) {
-        return computerChoice = 'Paper';
+    return rollDice;
+}
+
+function playerSelection() {
+choice = choice.toLowerCase();
+  if ( choice != null || choice != undefined ) {
+    if ( choice == 'rock' || choice == 'paper' || choice == 'scissors') {
+        return choice;
     } else {
-        return computerChoice = 'Scissors';
+        alert("Incorrect choice")
+    }
+} else {
+    alert("Enter again please!")
+}
+}
+
+function computerSelection() {
+    let rollDice = computerPlay();
+    if ( rollDice == 0 ) {
+        return computerChoice = 'rock';
+    } else if ( rollDice == 1) {
+        return computerChoice = 'paper';
+    } else {
+        return computerChoice = 'scissors';
     }
 }
 
+function playRound (player, computer) {
+    switch (true) {
+        case (player == 'rock' && computer == 'scissors'):
+            alert("You won! rock beats scissors");
+            break;
+        case (player == 'rock' && computer == 'paper'):
+            alert("You lost! paper beats rock");
+            break;
+        case (player == 'rock' && computer == 'rock'):
+            alert("You tied! rock cannot beats rock");
+            break;
+        case (player == 'paper' && computer == 'scissors'):
+            alert("You lost! scissors beats paper");
+            break;
+        case (player == 'paper' && computer == 'rock'):
+            alert("You won! paper beats rock");
+            break;
+        case (player == 'paper' && computer == 'paper'):
+            alert("You tied! paper cannot beat paper");
+            break;
+        case (player == 'scissors' && computer == 'scissors'):
+            alert("You tied! scissors cannot beat scissors");
+            break;
+        case (player == 'scissors' && computer == 'paper'):
+            alert("You won! scissors beats paper");
+            break;
+        case (player == 'scissors' && computer == 'rock'):
+            alert("You lost! scissors cannot beat rock");
+            break;
+        
+        default:
+            alert("Incorrect input!")
+            break;
+    }
+}
+
+playRound(choice,computer);
